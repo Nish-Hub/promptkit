514ae6c4928db8d2431dddf7644ded7ae635d009
BigO_Mojo <nishantgsingh07@gmail.com>
2026-01-01 20:12:03 +0530

    Deploying to github

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,11 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/promptkit/",
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-  server: {
-    port: 5173
-  }
 });
